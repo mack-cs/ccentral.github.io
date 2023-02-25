9c010fe1a738811f142948457c6d7bce37d6fc9a
Mackenzie Shonayi <shonayimack@gmail.com>
2023-02-25 23:24:27 +0200

    Add files via upload

---
+++ in.getclicky.com/in.php
@@ -0,0 +1,11 @@
+// (☞ﾟ∀ﾟ)☞ static33
+if( window._cgen ) {
+_cgen.set_cookie('_jsuid', '995201972');
+_cgen.advanced();
+_cgen.ping_start();
+_cgen.set_cookie('_heatmaps_g2g_100717247', 'no', 600 );
+_heatmaps_g2g_100717247='no';
+_cgen.set_cookie('_first_pageview', 1, 600 );
+_cgen.set_cookie('_referrer_og', 'https://www.google.com/', 30*86400 );
+_cgen.set_cookie('_utm_og', '&utm_source=cc_newsletter&utm_medium=email&utm_campaign=february2023', 30*86400 );
+}
